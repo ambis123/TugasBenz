0d772a9c212c64a9e1b76316c9929f8d1d552acb
ambis123 <38097225+ambis123@users.noreply.github.com>
2018-04-07 00:37:49 +0700

    Memanggil Method

--- src/tugasbenz/TugasBenzAksi.java
+++ src/tugasbenz/TugasBenzAksi.java
@@ -11,6 +11,20 @@ package tugasbenz;
  */
 public class TugasBenzAksi {
     public static void main(String[] args) { 
-        TugasBenz1 Abdi = new TugasBenz1(); 
+        TugasBenz1 Benny = new TugasBenz1();
+        
+      Benny.Nama="Benny Setiawan";
+      Benny.NPM="16630537";
+      Benny.Kelas="4A Nonreg";
+      Benny.Class="Hotel";
+      Benny.Objek="Hotel Bintang 3, Hotel Bintang 4,Hotel Bintang 5";
+      Benny.Atribut="Hotel Bintang 3= Harga : murah, Fasilitas: sedang,"
+              + "Total Lantai : 5 Lantai, Warna Bangunan: Hitam,"
+              + "Tempat Parkir: Kecil, Hotel Bintang 4= Harga, Mahal,"
+              + "Fasilitas: Mewah, Total Lantai : 7,Warna Bangunan: Biru"
+              + " Tempat Parkir: Luas,"
+              + "Hotel Bintang 5= Harga: Mahal, Fasilitas: Sangat Mewah,"
+              + "Total Lantai: 10, Warna Bangunan: Merah, Tempat Parkir: "
+              + "Sangat Luas";
 }
 }
